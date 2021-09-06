f68081e7c96883c3e1b92a729a6bdcc613354eb8
Stepan <always.prog@gmail.com>
2021-09-06 16:10:18 +0400

    make it for elite x crew

--- towny/functions.py
+++ towny/functions.py
@@ -17,74 +17,74 @@ def get_updates():
 
 
 def parse_desc(desc):
-    '''minetown'''
+    # '''minetown'''
+    # players = []
+    # town = ''
+    # mayor = ''
+    # nation = ''
+    # settings = ''
+    # parsed = BeautifulSoup(f'<html><body>{desc}</body></html>', 'html.parser')
+    # regioninfo = parsed.find('div', 'regioninfo')
+    # if not regioninfo:
+    #     return
+    # # getting mayor
+    # mayor = regioninfo.findAll('span')[2].findAll('span')[1].text.replace(' ', '')
+
+    # # getting town
+    # town = regioninfo.findAll('span')[0].text
+
+    # # getting nation
+    # nation = regioninfo.findAll('span')[7].text.replace('Нация: ', '')
+
+    # players_raw = regioninfo.findAll('span')[27]
+    # players_raw = players_raw.text.split(':')[1:][0]
+    # for player in players_raw.split(','):
+    #     players.append(player.replace(' ', ''))
+    # print(players)
+    # return {
+    #     'town': town,
+    #     'mayor': mayor,
+    #     'nation': nation,
+    #     'players': players,
+    #     'settings': settings
+    # }
+    # getting players
+
+    '''elitecrew'''
+
     players = []
     town = ''
     mayor = ''
     nation = ''
     settings = ''
-    parsed = BeautifulSoup(f'<html><body>{desc}</body></html>', 'html.parser')
-    regioninfo = parsed.find('div', 'regioninfo')
-    if not regioninfo:
-        return
+    parsed = BeautifulSoup(f'<html><body>{desc}</body></html>','html.parser')
+    infowindow = parsed.find('div','infowindow')
+    if not infowindow:
+    	return
+
     # getting mayor
-    mayor = regioninfo.findAll('span')[2].findAll('span')[1].text.replace(' ', '')
+    mayor = infowindow.findAll('span')[1].text
 
     # getting town
-    town = regioninfo.findAll('span')[0].text
+    town = get_town_name.match(infowindow.findAll('span')[0].text).group(1).replace(' ','')
 
     # getting nation
-    nation = regioninfo.findAll('span')[7].text.replace('Нация: ', '')
-
-    players_raw = regioninfo.findAll('span')[27]
-    players_raw = players_raw.text.split(':')[1:][0]
-    for player in players_raw.split(','):
-        players.append(player.replace(' ', ''))
-    print(players)
-    return {
-        'town': town,
-        'mayor': mayor,
-        'nation': nation,
-        'players': players,
-        'settings': settings
-    }
+    nation = get_nation_name.match(infowindow.findAll('span')[0].text).group(1).replace(' ','')
     # getting players
 
-    '''elitecrew'''
+    # getting settings
+    settings = infowindow.findAll('span')[3].text
 
+    for player in infowindow.findAll('span')[2].text.split(','):
+    	players.append(player.replace(' ',''))
 
-# town = ''
-# mayor = ''
-# nation = ''
-# settings = ''
-# parsed = BeautifulSoup(f'<html><body>{desc}</body></html>','html.parser')
-# infowindow = parsed.find('div','infowindow')
-# if not infowindow:
-# 	return
-
-# # getting mayor
-# mayor = infowindow.findAll('span')[1].text
-
-# # getting town
-# town = get_town_name.match(infowindow.findAll('span')[0].text).group(1).replace(' ','')
-
-# # getting nation
-# nation = get_nation_name.match(infowindow.findAll('span')[0].text).group(1).replace(' ','')
-# # getting players
-
-# # getting settings
-# settings = infowindow.findAll('span')[3].text
-
-# for player in infowindow.findAll('span')[2].text.split(','):
-# 	players.append(player.replace(' ',''))
-
-# return {
-# 'town': town,
-# 'mayor':mayor,
-# 'nation':nation,
-# 'players':players,
-# 'settings': settings
-# }
+    return {
+    'town': town,
+    'mayor':mayor,
+    'nation':nation,
+    'players':players,
+    'settings': settings
+    }
 
 def get_towns_info():
     updates = get_updates()['updates']
